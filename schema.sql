12e326625585b89b21a212f7bcb67dd5f15afdd0
James Jones <jjones@Jamess-MacBook-Pro.local>
2020-05-12 01:41:28 -0400

    initial sql for schema, users table and updated_at function

---
+++ schema.sql
@@ -0,0 +1,40 @@
+BEGIN;
+
+-- drop all existing tables
+DROP TABLE IF EXISTS td.users;
+
+-- drop schema
+DROP SCHEMA IF EXISTS td;
+
+-- drop all existing functions
+DROP FUNCTION IF EXISTS set_current_timestamp_updated_at();
+
+-- create schema
+CREATE SCHEMA IF NOT EXISTS td;
+
+-- create the table to store users
+CREATE TABLE td.users (
+    id              SERIAL PRIMARY KEY,
+    first_name      VARCHAR (30),
+    last_name       VARCHAR (30),
+    email           VARCHAR (30) NOT NULL UNIQUE,
+    token_version   INT DEFAULT 0,
+    created_at      TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+    updated_at      TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
+);
+
+-- function to update timestamp
+CREATE FUNCTION set_current_timestamp_updated_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$;
+
+-- trigger for updating td.users updated_at
+CREATE TRIGGER set_td_users_updated_at BEFORE UPDATE ON td.users FOR EACH ROW EXECUTE PROCEDURE set_current_timestamp_updated_at();
